62403988617ab412c098b4a044c624b7a8d214bf
Brandon Neil <brandon-neil@uiowa.edu>
2014-01-27 10:40:03 -0600

    Update trips file to correspond with shapes.txt

--- inc/trips.php
+++ inc/trips.php
@@ -9,6 +9,8 @@ function setTrip($trip, $route_name) {
   $data['route_id'] = $route_name;
   $data['service_id'] = "year_round";
   $data['trip_id'] = $route_name . "_" . $trip;
+  $data['shape_id'] = "shape_" . $route_name;
+
 
   //debug($data);
   return $data;
