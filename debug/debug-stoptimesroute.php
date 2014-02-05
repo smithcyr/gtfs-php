1785168e2156d4520ab740635a66a019a346fefc
Brandon Neil <brandon-neil@uiowa.edu>
2014-02-05 15:17:23 -0600

    Updated stop times generator file to include variants

--- debug/debug-stoptimesroute.php
+++ debug/debug-stoptimesroute.php
@@ -2,23 +2,29 @@
 ini_set("auto_detect_line_endings", true);
 include("../inc/stoptimesroute.php");
 
-$agency = 'iowa-city';
-$route = 'eventshuttle';
+$agency = 'coralville';
+$route = 'lantern';
+$service_id = "coralville_weekday";
+$startid = 1000;
+$route_variant = "lantern";
 
-$data = get_route_stop_times($agency, $route);
+$data = get_route_stop_times($agency, $route, $route_variant, $service_id, $startid);
 
 debug($data['trips']);
 
-debug($data['stoptimes']);
+
+
+//debug($data['stoptimes']);
 
 if(isset($data['stoptimes'])) {
-  unlink('exports/test/' . $route . '_stoptimes.csv');
-  $writer = new \EasyCSV\Writer('exports/test/' . $route . '_stoptimes.csv');
+  unlink('../exports/stoptimes/' . $agency . "_" . $route_variant . '_stoptimes.csv');
+  $writer = new \EasyCSV\Writer('../exports/stoptimes/' . $agency . "_" . $route_variant . '_stoptimes.csv');
+  //$writer->writeRow('trip_id, arrival_time, departure_time, stop_id, stop_sequence, stop_headsign');
   $writer->writeFromArray($data['stoptimes']);
 
-  unlink('exports/test/' . $agency . '_trips.csv');
-  $writer = new \EasyCSV\Writer('exports/test/' . $agency . '_trips.csv');
-  $writer->writeRow('route_id, service_id, trip_id');
+  unlink('../exports/trips/' . $agency . "_" . $route_variant. '_trips.csv');
+  $writer = new \EasyCSV\Writer('../exports/trips/' . $agency . "_" . $route_variant. '_trips.csv');
+  //$writer->writeRow('route_id, service_id, trip_id');
   $writer->writeFromArray($data['trips']);
 }
 
