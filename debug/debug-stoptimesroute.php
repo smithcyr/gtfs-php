f4819f6133c5c341a1c48bd81560e80e35945a91
Brandon Neil <brandon-neil@uiowa.edu>
2014-02-11 16:32:46 -0600

    added filename variable

--- debug/debug-stoptimesroute.php
+++ debug/debug-stoptimesroute.php
@@ -3,11 +3,12 @@ ini_set("auto_detect_line_endings", true);
 include("../inc/stoptimesroute.php");
 
 $agency = 'iowacity';
-$route = 'ndodge';
-$service_id = "iowacity_weekday";
-$startid = 1000;
-$route_variant = "ndodge_a";
-$shape_variant = "ndodge_a";
+$route = 'shuttles';
+$service_id = "iowacity_fallwinter";
+$startid = 4000;
+$route_variant = "shuttles_b";
+$shape_variant = "shuttles";
+$filename = "shuttles_b_fallwinter";
 
 $data = get_route_stop_times($agency, $route, $route_variant, $service_id, $startid, $shape_variant);
 
@@ -18,13 +19,13 @@ debug($data['trips']);
 //debug($data['stoptimes']);
 
 if(isset($data['stoptimes'])) {
-  unlink('../exports/stoptimes/' . $agency . "_" . $route_variant . '_stoptimes.csv');
-  $writer = new \EasyCSV\Writer('../exports/stoptimes/' . $agency . "_" . $route_variant . '_stoptimes.csv');
+  unlink('../exports/stoptimes/' . $agency . "_" . $filename . '_stoptimes.csv');
+  $writer = new \EasyCSV\Writer('../exports/stoptimes/' . $agency . "_" . $filename . '_stoptimes.csv');
   //$writer->writeRow('trip_id, arrival_time, departure_time, stop_id, stop_sequence, stop_headsign');
   $writer->writeFromArray($data['stoptimes']);
 
-  unlink('../exports/trips/' . $agency . "_" . $route_variant. '_trips.csv');
-  $writer = new \EasyCSV\Writer('../exports/trips/' . $agency . "_" . $route_variant. '_trips.csv');
+  unlink('../exports/trips/' . $agency . "_" . $filename. '_trips.csv');
+  $writer = new \EasyCSV\Writer('../exports/trips/' . $agency . "_" . $filename. '_trips.csv');
   //$writer->writeRow('route_id, service_id, trip_id');
   $writer->writeFromArray($data['trips']);
 }
